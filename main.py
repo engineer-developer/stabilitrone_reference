1e4af29cac367a6ec5f58a1e61303a051832ddd6
Alexey <suslov.lexx@gmail.com>
2024-04-15 20:44:40 +0500

    adapted to compile

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import csv
 import re
-from pathlib import Path
+import os
 import tkinter as tk
 from tkinter import Label, Entry, Button, messagebox, scrolledtext
 
 
-BASE_PATH = Path.cwd()
-CSV_FILE_PATH = BASE_PATH / "Stabilitrone.csv"
-
+CSV_FILE_PATH = os.path.join(".", "_internal", "DB", "Stabilitrone.csv")
+ICON_FILE = os.path.join(".", "_internal", "logo", "logo.ico")
 
 class StabilitroneDirectoryApp(tk.Tk):
     def __init__(self, storage: list[dict]):
         super().__init__()
         self.title('Справочник стабилитронов')
-        self.geometry('640x520')
+        self.geometry('640x480')
+        self.iconbitmap(ICON_FILE)
         self.storage = storage
 
         Label(self, text="Введите наименование стабилитрона:").pack(pady=10)
